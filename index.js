58f8ddf0f709c1092e021fca782d9f7ecdb04490
rhiokim <rhio.kim@gmail.com>
2017-01-22 18:14:16 +0900

    feat: add root api return and display initial message after run

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ const http = require('http')
 const path = require('path')
 const displayRoutes = require('express-routemap')
 
+const pkg = require('./package.json')
 const utils = require('./routes/utils')
 const docker = require('./routes/docker')
 const gorae = require('./routes/gorae')
 const socketServer = require('./lib/socketServer')
+
+const port = process.env.PORT || 8082
 
 const app = express()
 app.use(cors())
 app.use(express.static(path.join(__dirname, '..', 'terminal')))
 app.use(express.static(path.join(__dirname, '..', 'www')))
 
+app.get('/', (req, res) => {
+  res.send(`gorae server v${pkg.version}`)
+})
+
 app.use('/utils', utils)
 app.use('/api', docker)
 app.use('/gorae', gorae)
 
-const httpServer = http.createServer(app).listen(process.env.PORT || 8082, () => {
+const httpServer = http.createServer(app).listen(port, () => {
   displayRoutes(app)
+
+  console.log('start server localhost: %s', port)
 })
 socketServer(httpServer)
